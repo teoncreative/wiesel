aca10ee5a00e079ed7506687434f18a5c3f3fc96
Metehan Gezer <irrelevantdev@outlook.com>
2023-03-21 10:09:55 +0300

    Logger levels

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -6,19 +6,32 @@
 #include <iostream>
 
 namespace Wiesel {
+	LogLevel minLogLevel = LOG_LEVEL_DEBUG;
+	void setLogLevel(LogLevel level) {
+		minLogLevel = level;
+	}
+
 	void logDebug(const std::string& s) {
+		if (minLogLevel > LOG_LEVEL_DEBUG)
+			return;
 		std::cout << "[debug]: " << s << std::endl;
 	}
 
 	void logInfo(const std::string& s) {
-		std::cout << "[info]: " << s << std::endl;
-	}
-
-	void logError(const std::string& s) {
-		std::cerr << "[error]: " << s << std::endl;
+		if (minLogLevel > LOG_LEVEL_INFO)
+			return;
+		std::cout << "[info ]: " << s << std::endl;
 	}
 
 	void logWarn(const std::string& s) {
-		std::cerr << "[warn]: " << s << std::endl;
+		if (minLogLevel > LOG_LEVEL_WARN)
+			return;
+		std::cerr << "[warn ]: " << s << std::endl;
+	}
+
+	void logError(const std::string& s) {
+		if (minLogLevel > LOG_LEVEL_ERROR)
+			return;
+		std::cerr << "[error]: " << s << std::endl;
 	}
 }
