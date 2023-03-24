2de69e239f3710f08789539e80d870e2907da769
Metehan Gezer <irrelevantdev@outlook.com>
2023-03-24 09:52:07 +0300

    Move view to base object

--- src/w_baseobject.cpp
+++ src/w_baseobject.cpp
@@ -22,14 +22,25 @@ const glm::quat& WieselObject::getOrientation() {
 
 void WieselObject::rotate(float radians, float ax, float ay, float az) {
 	orientation *= glm::angleAxis(radians, glm::vec3(ax, ay, az));
+	updateView();
 }
 
 void WieselObject::move(float x, float y, float z) {
-	this->move(glm::vec3(x, y, z));
+	position += glm::vec3(x, y, z);
+	updateView();
 }
 
 void WieselObject::move(const glm::vec3& move) {
 	position += move;
+	updateView();
+}
+
+void WieselObject::updateView() {
+	localView = glm::translate(glm::mat4(1.0f), position) * glm::toMat4((const glm::quat&) orientation);
+}
+
+const glm::mat4& WieselObject::getLocalView() {
+	return localView;
 }
 
 
