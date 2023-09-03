023ff1ca675c9a9f1ea9c89323c5558a48261f70
Metehan Gezer <irrelevantdev@outlook.com>
2023-09-03 17:01:39 +0300

    script reloading (oof)

--- examples/demo/assets/scripts/TestBehavior.cs
+++ examples/demo/assets/scripts/TestBehavior.cs
@@ -4,19 +4,26 @@ public class TestBehavior : MonoBehavior
 {
 
     private TransformComponent transform;
+    private ulong a;
 
     public TestBehavior() {
+        EngineInternal.LogInfo("TestBehavior!");
     }
 
-    public void Start()
+    public void OnStart()
     {
         EngineInternal.LogInfo("Start!");
         transform = GetComponent<TransformComponent>();
+        a = 0;
+        b = 0;
+        transform.Position.X = 0.0f;
     }
 
-    public void Update()
+    public void OnUpdate(float deltaTime)
     {
-        EngineInternal.LogInfo($"{transform.Position.X}");
+        EngineInternal.LogInfo("Update! " + a + " " + b);
+        a += 1;
+        b += 1;
         transform.Position.X += 0.1f;
     }
 
--- wiesel/assets/scripts/components/TransformComponent.cs
+++ wiesel/assets/scripts/components/TransformComponent.cs
@@ -6,15 +6,15 @@ namespace WieselEngine
 {
     public class TransformComponent
     {
-        private uint entityId;
-        private ulong scenePtr;
+        private ulong behaviorPtr;
         private HandledVector3f position;
+        private HandledVector3f rotation;
+        private HandledVector3f scale;
 
         public Vector3f Position
         {
             get
             {
-                EngineInternal.LogInfo(position.GetType().FullName);
                 return position;
             }
             set
@@ -25,55 +25,146 @@ namespace WieselEngine
             }
         }
 
-        public TransformComponent(uint entityId, ulong scenePtr)
+        public TransformComponent(ulong behaviorPtr)
         {
-            this.entityId = entityId;
-            this.scenePtr = scenePtr;
+            this.behaviorPtr = behaviorPtr;
             this.position = new HandledVector3f(GetPositionX, SetPositionX, GetPositionY, SetPositionY, GetPositionZ, SetPositionZ);
+            this.rotation = new HandledVector3f(GetRotationX, SetRotationX, GetRotationY, SetRotationY, GetRotationZ, SetRotationZ);
+            this.scale = new HandledVector3f(GetScaleX, SetScaleX, GetScaleY, SetScaleY, GetScaleZ, SetScaleZ);
         }
 
         private float GetPositionX()
         {
-            return GetPositionX(entityId, scenePtr);
+            return GetPositionX(behaviorPtr);
         }
 
         private float GetPositionY()
         {
-            return GetPositionY(entityId, scenePtr);
+            return GetPositionY(behaviorPtr);
         }
 
         private float GetPositionZ()
         {
-            return GetPositionZ(entityId, scenePtr);
+            return GetPositionZ(behaviorPtr);
         }
 
         private void SetPositionX(float x)
         {
-            SetPositionX(entityId, scenePtr, x);
+            SetPositionX(behaviorPtr, x);
         }
 
         private void SetPositionY(float y)
         {
-            SetPositionY(entityId, scenePtr, y);
+            SetPositionY(behaviorPtr, y);
         }
 
         private void SetPositionZ(float z)
         {
-            SetPositionZ(entityId, scenePtr, z);
+            SetPositionZ(behaviorPtr, z);
+        }
+
+        private float GetRotationX()
+        {
+            return GetRotationX(behaviorPtr);
+        }
+
+        private float GetRotationY()
+        {
+            return GetRotationY(behaviorPtr);
+        }
+
+        private float GetRotationZ()
+        {
+            return GetRotationZ(behaviorPtr);
+        }
+
+        private void SetRotationX(float x)
+        {
+            SetRotationX(behaviorPtr, x);
+        }
+
+        private void SetRotationY(float y)
+        {
+            SetRotationY(behaviorPtr, y);
+        }
+
+        private void SetRotationZ(float z)
+        {
+            SetRotationZ(behaviorPtr, z);
+        }
+
+        private float GetScaleX()
+        {
+            return GetScaleX(behaviorPtr);
+        }
+
+        private float GetScaleY()
+        {
+            return GetScaleY(behaviorPtr);
+        }
+
+        private float GetScaleZ()
+        {
+            return GetScaleZ(behaviorPtr);
+        }
+
+        private void SetScaleX(float x)
+        {
+            SetScaleX(behaviorPtr, x);
+        }
+
+        private void SetScaleY(float y)
+        {
+            SetScaleY(behaviorPtr, y);
+        }
+
+        private void SetScaleZ(float z)
+        {
+            SetScaleZ(behaviorPtr, z);
         }
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        static extern void SetPositionX(uint entityId, ulong scenePtr, float x);
+        static extern void SetPositionX(ulong behaviorPtr, float x);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        static extern void SetPositionY(uint entityId, ulong scenePtr, float y);
+        static extern void SetPositionY(ulong behaviorPtr, float y);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        static extern void SetPositionZ(uint entityId, ulong scenePtr, float z);
+        static extern void SetPositionZ(ulong behaviorPtr, float z);
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        static extern float GetPositionX(uint entityId, ulong scenePtr);
+        static extern float GetPositionX(ulong behaviorPtr);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        static extern float GetPositionY(uint entityId, ulong scenePtr);
+        static extern float GetPositionY(ulong behaviorPtr);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        static extern float GetPositionZ(uint entityId, ulong scenePtr);
+        static extern float GetPositionZ(ulong behaviorPtr);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern void SetRotationX(ulong behaviorPtr, float x);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern void SetRotationY(ulong behaviorPtr, float y);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern void SetRotationZ(ulong behaviorPtr, float z);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern float GetRotationX(ulong behaviorPtr);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern float GetRotationY(ulong behaviorPtr);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern float GetRotationZ(ulong behaviorPtr);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern void SetScaleX(ulong behaviorPtr, float x);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern void SetScaleY(ulong behaviorPtr, float y);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern void SetScaleZ(ulong behaviorPtr, float z);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern float GetScaleX(ulong behaviorPtr);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern float GetScaleY(ulong behaviorPtr);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern float GetScaleZ(ulong behaviorPtr);
+
+
     }
 }
