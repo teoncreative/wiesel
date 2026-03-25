066de951e020b434b2270bf646874ae03e24dffb
Metehan Gezer <irrelevantdev@outlook.com>
2026-03-25 04:29:01 +0300

    update copyright

--- wiesel/include/util/w_gamepadcodes.hpp
+++ wiesel/include/util/w_gamepadcodes.hpp
@@ -1,4 +1,14 @@
 //
+//   Copyright 2026 Metehan Gezer
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+
+//
 // Gamepad button and axis codes.
 // Values match GLFW gamepad layout (which mirrors SDL GameController).
 //
